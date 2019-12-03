dbfa4f55f7ccdb06da70cd414e696e314ac19908
Thomas Craig <thomascraig@users.noreply.github.com>
2019-12-03 14:46:34 -0400

    integrated with firebase, signin page with Google, signin component added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC4W0yYTRQkbrLpiZWGgaRv24M1MbhHqms",
+    authDomain: "crown-db-d3298.firebaseapp.com",
+    databaseURL: "https://crown-db-d3298.firebaseio.com",
+    projectId: "crown-db-d3298",
+    storageBucket: "crown-db-d3298.appspot.com",
+    messagingSenderId: "811332597151",
+    appId: "1:811332597151:web:450a3b7b030cb7442bf9aa"
+  }
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-sign-out.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-sign-out'>
+        <SignIn />
+    </div>
+)
+
+export default SignInAndSignUpPage;
